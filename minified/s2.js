4caaa343710b44d62299865a147bfc7460733f97
Tom Tromey <tom@tromey.com>
2017-09-13 10:12:01 -0600

    "Minify" s2.js
    Rewrite s2.js to be all on one line, so we can try prettifying it to see
    what happens.

--- minified/s2.js
+++ minified/s2.js
@@ -1,13 +1,1 @@
-var f = require("./s1").f;
-
-function clickme() {
-  console.log("in clickme");
-  f();
-}
-
-window.clickme = clickme;
-
-window.loaded = function() {
-  var b = document.getElementById("button");
-  b.addEventListener("click", clickme);
-};
+var f = require("./s1").f; function clickme() { console.log("in clickme"); f(); } window.clickme = clickme; window.loaded = function() {  var b = document.getElementById("button");  b.addEventListener("click", clickme);};
